fdff6e4e3dcfaded0b98588cbf83e0cd6dddfa75
Duy Pham <duy.phamthe91@gmail.com>
2018-07-29 16:16:36 -0700

    Add landing page and make urls.py a symlink

---
+++ landing/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    context = {
+    }
+
+    return render(request, 'landing/index.html', context)
---
+++ saleor/urls.landing.py
@@ -0,0 +1,38 @@
+from django.conf import settings
+from django.conf.urls import include, url
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.views import serve
+from django.views.i18n import JavaScriptCatalog, set_language
+from graphene_django.views import GraphQLView
+
+from .account.urls import urlpatterns as account_urls
+from .cart.urls import urlpatterns as cart_urls
+from .checkout.urls import urlpatterns as checkout_urls
+from .core.sitemaps import sitemaps
+from .core.urls import urlpatterns as core_urls
+from .dashboard.urls import urlpatterns as dashboard_urls
+from .data_feeds.urls import urlpatterns as feed_urls
+from .graphql.api import schema
+from .order.urls import urlpatterns as order_urls
+from .page.urls import urlpatterns as page_urls
+from .product.urls import urlpatterns as product_urls
+from .search.urls import urlpatterns as search_urls
+
+handler404 = 'saleor.core.views.handle_404'
+
+urlpatterns = [
+    url(r'^$', include('landing.urls'))]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+        # static files (images, css, javascript, etc.)
+        url(r'^static/(?P<path>.*)$', serve)] + static(
+            settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.ENABLE_SILK:
+    urlpatterns += [
+        url(r'^silk/', include('silk.urls', namespace='silk'))]
